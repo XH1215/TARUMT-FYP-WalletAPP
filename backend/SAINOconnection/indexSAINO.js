a98be3168ba0cd10b5f4b7108eaaa26169896bb4
XH1215 <chinxuanhong5328@gmail.com>
2024-09-28 01:44:48 +0800

    Merged DB

--- backend/SAINOconnection/indexSAINO.js
+++
@@ -1,7 +0,0 @@
-// const appSAINO = require('./appSAINO');
-// const port = process.env.PORT_SAINO || 3001; // Different port for SAINO API
-
-// // Start server
-// appSAINO.listen(port, () => {
-//     console.log(`SAINO API server is running on http://localhost:${port}`);
-// });
